2e8f4eed3d48c5cd818d0ffcbbb21b97b6d6d0c6
xiaodi7598 <q18342117598@163.com>
2026-01-01 20:39:52 +0800

    Update 主脚本第二部分.lua

--- 主脚本第二部分.lua
+++ 主脚本第二部分.lua
@@ -793,31 +793,11 @@ TweenService:Create(mainContainer, TweenInfo.new(0.3), {
 
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
-
 Notification:Notify(
     {Title = "欢迎使用黑白脚本", Description = "请加入QQ群：946671668"},
     {OutlineColor = Color3.fromRGB(255, 255, 255), Time = 9, Type = "image"},
     {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(150, 150, 150)}
 )
-
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
-
-Notification:Notify(
-    {Title = "已开启反挂机", Description = "作者帮助你开启反挂机了"},
-    {OutlineColor = Color3.fromRGB(255, 255, 255), Time = 9, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(150, 150, 150)}
-)
-
--- 反挂机脚本保持不变
-local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-    wait(1)
-    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-end)
 
 local Window = WindUI:CreateWindow({
     Title = '<font color="#FF3333">黑</font><font color="#FF9933">白</font><font color="#FFFF33">脚</font><font color="#33FF33">本</font>    ',  
